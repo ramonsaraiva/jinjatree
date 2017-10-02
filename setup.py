b6e7411cabb34d81d35e8ea0f405233b1b9752ce
Ramon Saraiva <ramonsaraiva@gmail.com>
2017-10-02 16:40:30 -0300

    Added initial setup file and bin script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='jinjatree',
+      version='0.1.0',
+      description='Understand how your jinjas relate',
+      url='https://github.com/ramonsaraiva/jinjatree',
+      author='Ramon Saraiva',
+      author_email='ramonsaraiva@gmail.com',
+      license='MIT',
+      packages=['jinjatree'],
+      scripts=['bin/jinjatree'],
+      zip_safe=False)
