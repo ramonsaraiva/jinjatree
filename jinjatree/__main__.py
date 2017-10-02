a28816f8ca53e6c96cd778f7452211eab5194ade
Ramon Saraiva <ramonsaraiva@gmail.com>
2017-10-01 21:56:56 -0300

    Added proper logic to process the jinja template. Simple MVP that prints out the templates tree

--- jinjatree/__main__.py
+++ jinjatree/__main__.py
@@ -5,6 +5,7 @@ from core import JinjaTree
 
 if __name__ == '__main__':
     try:
-        JinjaTree(sys.argv[1])
+        tree = JinjaTree(sys.argv[1])
+        tree.render()
     except IndexError:
         print('Please provide a starter path for the jinja templates')
--- jinjatree/core.py
+++ jinjatree/core.py
@@ -15,8 +15,14 @@ class JinjaTree:
         self.nodes = []
         self.jinjas = []
         self.location = location
+        self.current_node = None
+        self.root = None
 
         self.initialize()
+
+    @property
+    def orphan_nodes(self):
+        return (node for node in self.nodes if node.is_root)
 
     def load_jinjas(self):
         for path, name, filenames in os.walk(self.location):
@@ -29,14 +35,42 @@ class JinjaTree:
     def process_content(self, content):
         matches = re.findall(self.RELATIONSHIP_PATTERN, content)
         for relationship, name in matches:
-            print((relationship, name))
+            node, created = self.find_or_create(name)
+            if relationship == 'extends':
+                self.current_node.parent = node
+                continue
+            if relationship == 'include':
+                if not created:
+                    node = Node(name)
+                node.parent = self.current_node
 
     def build_nodes(self):
         for path, jinja in self.jinjas:
+            self.current_node, _ = self.find_or_create(jinja)
             with open(path, 'r') as f:
                 content = f.read()
                 self.process_content(content)
 
+    def adopt_orphan_nodes(self):
+        self.root = Node('templates')
+        for node in self.orphan_nodes:
+            node.parent = self.root
+
+    def find_or_create(self, jinja):
+        found = (node for node in self.nodes if node.name == jinja)
+
+        try:
+            return next(found), False
+        except StopIteration:
+            node = Node(jinja)
+            self.nodes.append(node)
+            return node, True
+
     def initialize(self):
         self.load_jinjas()
         self.build_nodes()
+        self.adopt_orphan_nodes()
+
+    def render(self):
+        for pre, fill, node in RenderTree(self.root):
+            print(f'{pre}{node.name}')
