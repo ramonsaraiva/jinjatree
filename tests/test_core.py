6f28d52adc43158b63056dbcb05e9273399977b8
Ivan Neto <ivan.cr.neto@gmail.com>
2017-10-02 23:34:44 -0300

    added first tests

---
+++ tests/test_core.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+from jinjatree.core import JinjaTree, TreeRenderer
+
+
+class JinjaTreeTest(TestCase):
+
+    def setUp(self):
+        self.jinjatree = JinjaTree()
+
+    def test_attr(self):
+        self.assertEqual(
+            self.jinjatree.RELATIONSHIP_PATTERN,
+            r'(extends|include|from) [\"\'](.*?)[\"\']')
+        self.assertIsInstance(self.jinjatree, TreeRenderer)
